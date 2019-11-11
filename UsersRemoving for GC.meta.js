b2e32c58d32131b3e6512ebae8638ca33d125362
DmitrySpace <crazydimyan@mail.ru>
2019-11-11 03:41:58 +0300

    Add files via upload

---
+++ UsersRemoving for GC.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name UsersRemoving for GC
+// @description Удаляет пользователей по сегменту
+// @author      Dmitry Space
+// @version     1.0
+// @date        2019-11-11
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.12.0/js/md5.min.js
+// @include *://*/*
+// ==/UserScript==
+/////////////////////////
